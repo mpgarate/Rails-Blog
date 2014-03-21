8fb849c5fb81d455623f93c7c5bb3ca72c2c7f01
vagrant <mpgarate@gmail.com>
2014-03-21 14:08:23 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MgBlog::Application.config.secret_key_base = '108004a11b75860a26ddaf23cb93e7e5fdd0fe4431600734416d5dac8b50db3b39bf5eaa33b1031452edffcb4e0615dc9225f8ae1ae84ada7ab1c1d0b0b30946'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MgBlog::Application.config.session_store :cookie_store, key: '_mg_blog_session'
